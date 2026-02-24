62303c2a436a1ff50f8c327a11fb4633543ab5ba
Szabó Dániel <szabo.daniel@euroone.hu>
2026-02-24 14:16:06 +0100

    Try catch added

--- csharp/2026-02-21/2026-02-21/Program.cs
+++ csharp/2026-02-21/2026-02-21/Program.cs
@@ -6,23 +6,31 @@ namespace _2026_02_21
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person("Person One", 28);
-            Student s1 = new Student("Student One", 27, "XUXEJO");
-            Student s2 = new Student("Student Two", 37, "XUXEJU");
-            Student s3 = new Student("Student Three", 19, "XUXEJA");
-            Employee e1 = new Employee("Employee One", 49, 380000);
-            
-            var students = new List<Student>();
-            students.Add(s1);
-            students.Add(s2);
-            students.Add(s3);
-
-            Console.WriteLine(p1);
-            foreach (var student in students)
+            try
             {
-                Console.WriteLine(student.ToString());
+                Person p1 = new Person("Person One", 28);
+                Student s1 = new Student("Student One", 27, "XUXEJOasd");
+                Student s2 = new Student("Student Two", 37, "XUXEJU");
+                Student s3 = new Student("Student Three", 19, "XUXEJA");
+                Employee e1 = new Employee("Employee One", 49, 380000);
+                
+                var students = new List<Student>();
+                students.Add(s1);
+                students.Add(s2);
+                students.Add(s3);
+
+                Console.WriteLine(p1);
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student.ToString());
+                }
+                Console.WriteLine(e1);
             }
-            Console.WriteLine(e1);
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error has occured: {e}");
+                throw;
+            }
         }
     }
 }
