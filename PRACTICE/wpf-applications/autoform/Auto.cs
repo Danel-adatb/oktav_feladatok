95f9c1a1892bde1067ca4bd7253b11e1606566f5
Szabó Dániel <szabo.daniel@euroone.hu>
2026-09-24 16:50:54 +0200

    WPF autoform added

---
+++ PRACTICE/wpf-applications/autoform/Auto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace autoform
+{
+    internal class Auto
+    {
+
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public int SoldCount { get; set; }
+        public int AvgSoldPrice { get; set; }
+
+        public Auto(string row)
+        {
+            string[] data = row.Split([';'], StringSplitOptions.RemoveEmptyEntries);
+
+            this.Id = Int32.Parse(data[0]);
+            this.Brand = data[1];
+            this.Model = data[2];
+            this.Year = Int32.Parse(data[3]);
+            this.Color = data[4];
+            this.SoldCount = Int32.Parse(data[5]);
+            this.AvgSoldPrice = Int32.Parse(data[6]);
+        }
+
+        public override string ToString()
+        {
+            return $"Sorszám: {this.Id}\n" +
+                $"Márka: {this.Brand}\n" +
+                $"Modell: {this.Model}\n" +
+                $"Évjárat: {this.Year}\n" +
+                $"Szín: {this.Color}\n" +
+                $"Eladott DB: {this.SoldCount}\n" +
+                $"Átlag eladási ár: {this.AvgSoldPrice}\n" +
+                $"-----------------";
+        }
+
+        public static List<Auto> readData(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                List<Auto> cars = new List<Auto>();
+                reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line)) continue;
+                    cars.Add(new Auto(line));
+                }
+
+                return cars;
+            }
+        }
+    }
+}
---
+++ PRACTICE/wpf-applications/autoform/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Win32;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace autoform
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Auto> cars = new List<Auto>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+                "Biztosan ki szeretnél lépni?",
+                "Kilépés",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            );
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void loadBtn_Click(Object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Title = "CSV fájl megnyitása",
+                Filter = "CSV fájlok (*.csv)|*.csv|Minden fájl (*.*)|*.*",
+                DefaultExt = ".csv"
+            };
+
+            if(ofd.ShowDialog() == true )
+            {
+                string filePath = ofd.FileName;
+                List<Auto> datas = Auto.readData(filePath);
+                AutokDataGrid.ItemsSource = datas;
+                foreach( Auto data in datas )
+                {
+                    cars.Add( data );
+                }
+            }
+            
+        }
+
+        private void inputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // ListBox törlése
+            resultListBox.Items.Clear();
+
+            // Ha nincs még adat betöltve
+            if (cars.Count == 0)
+            {
+                return;
+            }
+
+            // Ha nem érvényes év
+            if (!int.TryParse(inputTextBox.Text, out int year))
+            {
+                return;
+            }
+
+            // Év alapján szűrés
+            var result = cars
+                .Where(car => car.Year == year)
+                .Select(car => $"{car.Brand} {car.Model}")
+                .ToList();
+
+            // ListBox feltöltése
+            foreach (string car in result)
+            {
+                resultListBox.Items.Add(car);
+            }
+        }
+    }
+}
